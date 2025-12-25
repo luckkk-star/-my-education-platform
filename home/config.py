e30d49804dd3d21a5c2e0425310949f7d3e19c7e
luckkk-star <3282292424@qq.com>
2025-12-25 22:13:00 +0800

    Delete home directory

--- home/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-for-development'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '520413LDld'
-    MYSQL_DB = 'homework'
-    MYSQL_CURSORCLASS = 'DictCursor'
